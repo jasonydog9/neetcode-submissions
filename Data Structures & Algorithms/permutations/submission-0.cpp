5c2068423f63cafa8cf0def8f52674245fdd194c
Jason Yap <82182700+jasonydog9@users.noreply.github.com>
2026-07-23 23:10:49 -0500

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> subset;
+        set<int> s;
+        dfs(nums, subset, s);
+        return res;
+    }
+
+    void dfs(vector<int>& nums, vector<int> subset, set<int> s)
+    {
+        if (subset.size() == nums.size())
+        {
+            res.push_back(subset);
+            return;
+        }
+
+        set<int> s_old = s;
+        vector<int> old_subset = subset;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (s.contains(nums[i]))
+            {
+                continue;
+            }
+            s.insert(nums[i]);
+            subset.push_back(nums[i]);
+            dfs(nums, subset, s);
+            s.erase(nums[i]);
+            subset.pop_back();
+        }
+    }
+};
