894bd63070556448b1d7b7603c5e84fa1a8575fa
Jason Yap <82182700+jasonydog9@users.noreply.github.com>
2026-07-24 22:28:33 -0500

    Add: search-for-word - submission-5

---
+++ Data Structures & Algorithms/search-for-word/submission-5.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        for (int i = 0; i < board.size(); i++)
+        {
+            for (int j = 0; j < board[i].size(); j++)
+            {
+                set<pair<int,int>> s;
+                if (backtrack(board, 0, {i,j}, s, word))    
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    bool backtrack(vector<vector<char>>& board, int index, pair<int,int> c, set<pair<int,int>> s, string word)
+    {
+        int row = c.first;
+        int col = c.second;
+        if (s.contains(c) || row < 0 || row >= board.size() || 
+        col < 0 || col >= board[board.size() - 1].size())
+            return false;
+        
+        if (word[index] != board[row][col])
+            return false;
+        s.insert(c);
+        if (index == word.size() - 1)
+            return true;
+
+        pair<int,int> new_pair(row + 1, col);
+        bool res1 = backtrack(board, index + 1, new_pair, s, word);
+        new_pair = {row - 1, col};
+        bool res2 = backtrack(board, index + 1, new_pair, s, word);
+        new_pair = {row, col -1};
+        bool res3 = backtrack(board, index + 1, new_pair, s, word);
+        new_pair = {row, col + 1};
+        bool res4 = backtrack(board, index + 1, new_pair, s, word);
+
+        s.erase(c);
+        return res1 || res2 || res3 || res4;
+
+
+
+
+
+    }
+};
