13432df8e8f149275357267197b8432827cbf141
Jason Yap <82182700+jasonydog9@users.noreply.github.com>
2026-09-21 15:42:52 -0500

    Add: merge-intervals - submission-3

---
+++ Data Structures & Algorithms/merge-intervals/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> ans;
+        ans.push_back(intervals[0]);
+
+        for (int i = 1; i < intervals.size(); i++)
+        {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            int lastEnd = ans.back()[1];
+
+            if (start <= lastEnd)
+            {
+                ans.back()[1] = std::max(lastEnd, end);
+            }
+            else
+            {
+                ans.push_back({start, end});
+            }
+        }
+
+        return ans;
+    }
+};
